54fb389e2a5d7172ac19031c7c3ece809e4e1e3e
branco772 <eduararratia@gmail.com>
2024-09-10 11:44:40 -0400

    login completo

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from .forms import CustomUserCreationForm
+from django.contrib.auth import authenticate, login
 # Create your views here.
 def home(request):
     return render(request, 'core/home.html')
@@ -18,4 +19,11 @@ def register(request):
     data={
         'form': CustomUserCreationForm()
     }
+    if request.method=='POST':
+        userCreationForm=CustomUserCreationForm(data=request.POST)
+        if userCreationForm.is_valid():
+            userCreationForm.save()
+            user=authenticate(username=userCreationForm.cleaned_data['username'], password=userCreationForm.cleaned_data['password1'])
+            login(request, user)
+            return redirect('home')
     return render(request, 'registration/register.html', data)
